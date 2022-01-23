b77c8df746b924292c124824f83c772aa46df5b3
keldorne <k.perreaut@gmail.com>
2022-01-23 14:21:39 +0100

    Mode test ajouté

--- main.py
+++ main.py
@@ -15,17 +15,24 @@ import shutil
 
 # Variable globale pour gérer les étapes dans l'ordre
 global step_one, step_two, step_three, numero_patient, nom_maison_retraite, nom_patient, prenom_patient, nom_accompagnant, prenom_accompagnant, telephone_accompagnant, mail_accompagnant, app3
-global chemin_calisto, chemin_calisto_barre, dossier_sauvegarde, la_date_jour_save
+global chemin_calisto, chemin_calisto_barre, dossier_sauvegarde, la_date_jour_save, choix_mode
 #
+
+# Implémentation d'un mode test qui marche sur d'autre machine n'ayant ni l'arborescence ni les fichiers prérequis
+# Normal où test
+choix_mode = ""
+#choix_mode = "test"
 
 ####
 # Path à modifier en fonction de votre configuration
-chemin_calisto = r"C:\\Users\\Audio69\\Documents\\DocumentCalisto"
-chemin_liste_referent = r"C:\\Users\\Audio69\\Documents\\DocOdipro\\DocMaster\\Liste Referents.xlsx"
-dossier_sauvegarde = rf"C:\\Users\\Audio69\\Documents\\DocOdipro\\SynologyDrive\\Depistages"
-os.makedirs(chemin_calisto)
-os.makedirs(chemin_liste_referent)
-os.makedirs(dossier_sauvegarde)
+if choix_mode == "test":
+    chemin_calisto = rf"{os.getcwd()}\mode_test\DocumentCalisto"
+    chemin_liste_referent = rf"{os.getcwd()}\mode_test\fichier_test\Liste Referents.xlsx"
+    dossier_sauvegarde = rf"{os.getcwd()}\mode_test\Depistages"
+else:
+    chemin_calisto = r"C:\\Users\\Audio69\\Documents\\DocumentCalisto"
+    chemin_liste_referent = r"C:\\Users\\Audio69\\Documents\\DocOdipro\\DocMaster\\Liste Referents.xlsx"
+    dossier_sauvegarde = rf"C:\\Users\\Audio69\\Documents\\DocOdipro\\SynologyDrive\\Depistages"
 ####
 
 
@@ -93,46 +100,49 @@ def press_on(key):
             prenom_accompagnant = app2.prenom_accompagnant
             telephone_accompagnant = app2.telephone_accompagnant
             mail_accompagnant = app2.mail_accompagnant
-            # Première étape on récupère nom prénom et on allume Calisto
-            # Clic fichier
-            pyautogui.click(42, 34)
-            time.sleep(duree)
-            # Clic Ajouter nouveau patient
-            pyautogui.click(118, 99)
-            time.sleep(duree)
-            # Clic Nom patient + remplissage
-            pyautogui.click(750, 232)
-            time.sleep(duree)
-            keyboard.write(nom_patient)
-            time.sleep(duree)
-            # CLic Prénom patient + remplissage
-            pyautogui.click(1149, 232)
-            time.sleep(duree)
-            keyboard.write(prenom_patient)
-            time.sleep(duree)
-            # Selection sexe patient
-            if app2.sexe_patient == "Homme":
-                pyautogui.click(1153, 267)
+            if choix_mode == "test":
+                pass
             else:
-                pyautogui.click(1239, 268)
-            time.sleep(duree)
-            # On enregistre la fiche patient
-            pyautogui.click(1111, 874)
-            time.sleep(duree)
-            # On clique sur la barre de recherche
-            pyautogui.click(22, 171)
-            time.sleep(duree)
-            # On colle le prénom et le nom du patient
-            prenom_nom_patient = [prenom_patient +"  " +nom_patient]
-            keyboard.write(prenom_nom_patient)
-            time.sleep(0.6)
-            # Selection fiche
-            pyautogui.click(52, 340)
-            time.sleep(0.6)
-            pyautogui.click(118, 337)
-            time.sleep(0.6)
-            # On ouvre le programme Calisto
-            pyautogui.click(103, 74)
+                # Première étape on récupère nom prénom et on allume Calisto
+                # Clic fichier
+                pyautogui.click(42, 34)
+                time.sleep(duree)
+                # Clic Ajouter nouveau patient
+                pyautogui.click(118, 99)
+                time.sleep(duree)
+                # Clic Nom patient + remplissage
+                pyautogui.click(750, 232)
+                time.sleep(duree)
+                keyboard.write(nom_patient)
+                time.sleep(duree)
+                # CLic Prénom patient + remplissage
+                pyautogui.click(1149, 232)
+                time.sleep(duree)
+                keyboard.write(prenom_patient)
+                time.sleep(duree)
+                # Selection sexe patient
+                if app2.sexe_patient == "Homme":
+                    pyautogui.click(1153, 267)
+                else:
+                    pyautogui.click(1239, 268)
+                time.sleep(duree)
+                # On enregistre la fiche patient
+                pyautogui.click(1111, 874)
+                time.sleep(duree)
+                # On clique sur la barre de recherche
+                pyautogui.click(22, 171)
+                time.sleep(duree)
+                # On colle le prénom et le nom du patient
+                prenom_nom_patient = [prenom_patient +"  " +nom_patient]
+                keyboard.write(prenom_nom_patient)
+                time.sleep(0.6)
+                # Selection fiche
+                pyautogui.click(52, 340)
+                time.sleep(0.6)
+                pyautogui.click(118, 337)
+                time.sleep(0.6)
+                # On ouvre le programme Calisto
+                pyautogui.click(103, 74)
             # On valide la première étape
             step_one = 1
 
@@ -141,113 +151,101 @@ def press_on(key):
             # Ouverture de la fenetre graphique
             app3 = FenetreAnamnese()
             app3.mainloop()
-
-            # Ouverture note
-            pyautogui.click(157, 587)
-            # Placement correct de la fenêtre
-            pyautogui.moveTo(941, 208, 0.1)
-            pyautogui.dragTo(317, 411, 0.2, button='left')
-            pyautogui.click(8, 463)
-
-            # On transforme les résultats obtenus avec le GUI en texte à copier dans le calisto pour le rapport
-            # On integre le nom de la maison de retraite au debut du texte
-            text_a_copier = "Etablissement : " + app1.nom_maison_retraite + "\n" + "\n"
-
-            # Ligne cognition
-            text_a_copier = text_a_copier + "   " + app3.les_texts[0 ] +"  " +app3.text_reponse[0 ] +"\n"
-
-            # Ligne déjà appareillé
-            if app3.text_reponse[1] == "":
+            if choix_mode == "test":
                 pass
             else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[1] + " " + app3.text_reponse[1] + "\n"
+                # Ouverture note
+                pyautogui.click(157, 587)
+                # Placement correct de la fenêtre
+                pyautogui.moveTo(941, 208, 0.1)
+                pyautogui.dragTo(317, 411, 0.2, button='left')
+                pyautogui.click(8, 463)
 
-            # Ligne otoscopie
-            if app3.text_reponse[2] == "" and app3.text_reponse[3] == "":
-                pass
-            elif app3.text_reponse[2] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[3] + "\n"
-            elif app3.text_reponse[3] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[2] + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[2] + ",  " + \
+                # On transforme les résultats obtenus avec le GUI en texte à copier dans le calisto pour le rapport
+                # On integre le nom de la maison de retraite au debut du texte
+                text_a_copier = "Etablissement : " + app1.nom_maison_retraite + "\n" + "\n"
+
+                # Ligne cognition
+                text_a_copier = text_a_copier + "   " + app3.les_texts[0 ] +"  " +app3.text_reponse[0 ] +"\n"
+
+                # Ligne déjà appareillé
+                if app3.text_reponse[1] == "":
+                    pass
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[1] + " " + app3.text_reponse[1] + "\n"
+
+                # Ligne otoscopie
+                if app3.text_reponse[2] == "" and app3.text_reponse[3] == "":
+                    pass
+                elif app3.text_reponse[2] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[3] + "\n"
+                elif app3.text_reponse[3] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[2] + "\n"
+                else:
+                 text_a_copier = text_a_copier + "   " + app3.les_texts[2] + " " + app3.text_reponse[2] + ",  " + \
                                 app3.text_reponse[3] + "\n"
 
-            # Ligne prise d'empreinte
-            if app3.text_reponse[4] == "" and app3.text_reponse[5] == "":
-                pass
-            elif app3.text_reponse[4] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[5] + "\n"
-            elif app3.text_reponse[5] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[4] + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[4] + ",  " + \
+                # Ligne prise d'empreinte
+                if app3.text_reponse[4] == "" and app3.text_reponse[5] == "":
+                    pass
+                elif app3.text_reponse[4] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[5] + "\n"
+                elif app3.text_reponse[5] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[4] + "\n"
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[3] + " " + app3.text_reponse[4] + ",  " + \
                                 app3.text_reponse[5] + "\n"
 
-            # Ligne remarque
-            if app3.text_reponse[6] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[4] + " Aucune" + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[4] + " " + app3.text_reponse[6] + "\n"
+                # Ligne remarque
+                if app3.text_reponse[6] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[4] + " Aucune" + "\n"
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[4] + " " + app3.text_reponse[6] + "\n"
 
-            # Ligne avis
-            if app3.text_reponse[7] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[5] + " Impossible de receuillir une réponse" + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[5] + " " + app3.text_reponse[7] + "\n"
+                # Ligne avis
+                if app3.text_reponse[7] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[5] + " Impossible de receuillir une réponse" + "\n"
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[5] + " " + app3.text_reponse[7] + "\n"
 
-            # Ligne conseil d'appareillage
-            if app3.text_reponse[8] == "" and app3.text_reponse[9] == "":
-                pass
-            elif app3.text_reponse[8] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[9] + "\n"
-            elif app3.text_reponse[9] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[8] + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[8] + ",  " + \
+                # Ligne conseil d'appareillage
+                if app3.text_reponse[8] == "" and app3.text_reponse[9] == "":
+                    pass
+                elif app3.text_reponse[8] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[9] + "\n"
+                elif app3.text_reponse[9] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[8] + "\n"
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[6] + " " + app3.text_reponse[8] + ",  " + \
                                     app3.text_reponse[9] + "\n"
 
-            # Ligne choix dome embout
-            if app3.text_reponse[10] == "" and app3.text_reponse[11] == "":
-                pass
-            elif app3.text_reponse[10] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[11] + "\n"
-            elif app3.text_reponse[11] == "":
-                text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[10] + "\n"
-            else:
-                text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[10] + ",  " + \
+                # Ligne choix dome embout
+                if app3.text_reponse[10] == "" and app3.text_reponse[11] == "":
+                    pass
+                elif app3.text_reponse[10] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[11] + "\n"
+                elif app3.text_reponse[11] == "":
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[10] + "\n"
+                else:
+                    text_a_copier = text_a_copier + "   " + app3.les_texts[7] + " " + app3.text_reponse[10] + ",  " + \
                                     app3.text_reponse[11] + "\n"
 
-            print (text_a_copier)
-            pyautogui.click(5, 461)
-            # pyautogui.dragTo(650, 1110, 0.2, button='left')
-            keyboard.write(text_a_copier)
+                print (text_a_copier)
+                pyautogui.click(5, 461)
+                # pyautogui.dragTo(650, 1110, 0.2, button='left')
+                keyboard.write(text_a_copier)
             step_two = 1
         else:
             print("Il faut compléter la premiere étape où finir la troisième étape")
 
     if key == Key.f11:
         if step_one == 1 and step_two == 1 and step_three == 0:
-            # on ferme la fiche note
-            pyautogui.click(709, 411)
-            time.sleep(0.1)
-            # On valide la confirmation d'enregistrement dans le cas où on revient sur la fiche
-            pyautogui.click(928, 566)
-            time.sleep(0.1)
-            # on enregistre le rapport en pdf
-            pyautogui.click(122, 45)
-            time.sleep(0.1)
-            # on enregistre la session
-            pyautogui.click(148, 45)
-            time.sleep(2)
-            # On verifie que le rapport est a bien été enregistré
-            if os.path.exists(chemin_calisto_barre + "Report.pdf"):
-                # on modifie le nom du pdf generé avec le nom du patient
+            if choix_mode == "test":
+                # On copie le rapport test dans le dossier Calisto
+                shutil.move(rf"{os.getcwd()}\mode_test\fichier_test\Report.pdf", rf"{os.getcwd()}\mode_test\DocumentCalisto\Report.pdf")
                 file_oldname = chemin_calisto_barre + "Report.pdf"
                 file_newname_newfile = rf"{chemin_calisto}\\{nom_maison_retraite}\\{nom_patient} {prenom_patient} {nom_maison_retraite}.pdf"
                 os.rename(file_oldname, file_newname_newfile)
-                # on ferme la session
-                pyautogui.click(1897, 12)
                 # on met à jour le fichier excel avec identité patient et accompagnant
                 ws.cell(row=numero_patient + 3, column=2).value = nom_patient
                 ws.cell(row=numero_patient + 3, column=3).value = prenom_patient
@@ -269,7 +267,48 @@ def press_on(key):
                 step_two = 0
                 numero_patient = numero_patient + 1
             else:
-                print("Le fichier à mal été enregistré")
+                # on ferme la fiche note
+                pyautogui.click(709, 411)
+                time.sleep(0.1)
+                # On valide la confirmation d'enregistrement dans le cas où on revient sur la fiche
+                pyautogui.click(928, 566)
+                time.sleep(0.1)
+                # on enregistre le rapport en pdf
+                pyautogui.click(122, 45)
+                time.sleep(0.1)
+                # on enregistre la session
+                pyautogui.click(148, 45)
+                time.sleep(2)
+                # On verifie que le rapport est a bien été enregistré
+                if os.path.exists(chemin_calisto_barre + "Report.pdf"):
+                    # on modifie le nom du pdf generé avec le nom du patient
+                    file_oldname = chemin_calisto_barre + "Report.pdf"
+                    file_newname_newfile = rf"{chemin_calisto}\\{nom_maison_retraite}\\{nom_patient} {prenom_patient} {nom_maison_retraite}.pdf"
+                    os.rename(file_oldname, file_newname_newfile)
+                    # on ferme la session
+                    pyautogui.click(1897, 12)
+                    # on met à jour le fichier excel avec identité patient et accompagnant
+                    ws.cell(row=numero_patient + 3, column=2).value = nom_patient
+                    ws.cell(row=numero_patient + 3, column=3).value = prenom_patient
+                    ws.cell(row=numero_patient + 3, column=5).value = nom_accompagnant
+                    ws.cell(row=numero_patient + 3, column=6).value = prenom_accompagnant
+                    ws.cell(row=numero_patient + 3, column=7).value = telephone_accompagnant
+                    ws.cell(row=numero_patient + 3, column=8).value = mail_accompagnant
+                    # on indique si il y a des empreintes
+                    if app3.text_reponse[4] == "" and app3.text_reponse[5] == "":
+                        ws.cell(row=numero_patient + 3, column=9).value = "Aucune"
+                    elif app3.text_reponse[4] == "":
+                        ws.cell(row=numero_patient + 3, column=9).value = "OD"
+                    elif app3.text_reponse[5] == "":
+                        ws.cell(row=numero_patient + 3, column=9).value = "OG"
+                    else:
+                        ws.cell(row=numero_patient + 3, column=9).value = "ODG"
+                    # On incrémente les variables
+                    step_one = 0
+                    step_two = 0
+                    numero_patient = numero_patient + 1
+                else:
+                    print("Le fichier à mal été enregistré")
         else:
             print("il faut compléter les étapes précédentes")
 
--- readme.md
+++ readme.md
@@ -11,3 +11,5 @@ partie audiomètre
 ->Il faut configurer dans calisto l'impression pdf par défaut menu/règlage/paramètre généraux/impression pdf
 ->Il faut un dossier DocumentsCalisto à l'endroit du chemin_calisto où modifier le path en conséquence
 Le dossier DocumentCalisto doit contenir un dossier au nom de l'EHPAD pour le bon fonctionnement du programme
+
+->Implémentation d'un mode test qui permet d'exécuter le programme dans l'arborescence mode_test
